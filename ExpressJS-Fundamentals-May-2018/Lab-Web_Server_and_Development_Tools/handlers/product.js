6dd269138925eb9d6a628f92a8efb88ccc0d5439
IvayloIV <ivvo98@abv.bg>
2019-01-03 12:08:03 +0200

    Add Lab-Web Server and Development Tools

---
+++ ExpressJS-Fundamentals-May-2018/Lab-Web_Server_and_Development_Tools/handlers/product.js
@@ -0,0 +1,88 @@
+const url = require('url');
+const database = require('../config/database');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+const multiparty = require('multiparty');
+const shortid = require('shortid');
+
+module.exports = (req, res) => {
+    req.pathname = req.pathname || url.parse(req.url).pathname;
+
+    if (req.pathname === '/product/add' && req.method === 'GET') {
+        let filePath = path.normalize(
+            path.join(__dirname, '../views/products/add.html')
+        );
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('404 not found!');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.pathname === '/product/add' && req.method === 'POST') {
+        let form = new multiparty.Form();
+        let product = {};
+
+        form.on('part', (part) => {
+            if (part.filename) {
+                let dataString = '';
+
+                part.setEncoding('binary');
+                part.on('data', (data) => {
+                    dataString += data;
+                });
+
+                part.on('end', () => {
+                    let fileName = shortid.generate();
+                    let filePath = `/content/images/${fileName}.jpg`;
+
+                    product.image = filePath;
+                    fs.writeFile(
+                        `.${filePath}`, dataString, {encoding: 'ascii'}, (err) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                        }
+                    );
+                });
+            } else {
+                part.setEncoding('utf-8');
+                let field = '';
+                part.on('data', (data) => {
+                    field += data;
+                });
+
+                part.on('end', () => {
+                    product[part.name] = field;
+                });
+            }
+        });
+
+        form.on('close', () => {
+            database.products.add(product);
+
+            res.writeHead(302, {
+                Location: '/'
+            });
+
+            res.end();
+        });
+
+        form.parse(req);
+    } else {
+        return true;
+    }
+}
---
+++ ExpressJS-Fundamentals-May-2018/Lab-Web_Server_and_Development_Tools/handlers/static-files.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+function getContentType(url) {
+    if (url.endsWith('.css')) {
+        return 'text/css';
+    } else if (url.endsWith('.ico')) {
+        return 'image/x-icon';
+    } else if (url.endsWith('.jpg')) {
+        return 'image/jpg';
+    }
+}
+
+module.exports = (req, res) => {
+    req.pathname = req.pathname || url.parse(req.url).pathname;
+
+    if (req.pathname.startsWith('/content/') && req.method === "GET") {
+        let filePath = path.normalize(
+            path.join(__dirname, `..${req.pathname}`));
+            
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('Resource not found!');
+                res.end();
+                return;
+            } 
+
+            res.writeHead(200, {
+                'Content-Type': getContentType(req.pathname)
+            });
+
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+}
